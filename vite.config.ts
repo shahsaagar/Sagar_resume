a9e86fdb3df12f934f2bfc48b412353634b2fbbb
shahsaagar <shahsaagar@gmail.com>
2025-11-30 19:08:25 +0530

    My Resume

---
+++ vite.config.ts
@@ -0,0 +1,9 @@
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  base: '/Sagar_resume', // ⚠ Replace with your GitHub repo name
+  plugins: [react()],
+});
+
